db8024ddb406d65f9b15539c0182c9abc49be24a
Geoff Storbeck <storbeck@gmail.com>
2015-11-21 13:30:55 -0500

    updated arguments-optional to pass additional tests

--- arguments-optional.js
+++ arguments-optional.js
@@ -1,17 +1,16 @@
-function add(x) { 
-    var args = Array.prototype.slice.call(arguments, 0); 
-    if (x + 0 === x) { 
-        if (args.length === 2) { 
-            return args[0] + args[1]; 
-        } else { 
-            return function(y) { 
-                return x + y; 
-            }; 
-            
-        } 
-        
-    } 
-    
-} 
+function add(x) {
+  var args = Array.prototype.slice.call(arguments, 0);
+
+  if (typeof x === 'number') {
+    if (args.length === 2) {
+      if (typeof args[1] === 'number') return args[0] + args[1];
+    } else {
+      return function(y) {
+        if (typeof y === 'number') return x + y;
+      };
+
+    }
+  }
+}
 
 add(2,3);
