6708df302d3b0367b36755377a83d973b83c8725
Geoff Storbeck <storbeck@gmail.com>
2015-12-08 18:49:15 -0500

    shortened the roman numeral converter

--- Roman_Numeral_Converter.js
+++ Roman_Numeral_Converter.js
@@ -1,14 +1,7 @@
 function convert(num) {
-    var r = '',
-        decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
-        roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-    for (var i = 0; i < decimals.length; i++) {
-        while (num >= decimals[i]) {
-            r += roman[i];
-            num -= decimals[i];
-        }
-    }
-    return r;
+    var R = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    if (num < 1) return '';
+    for (var k in R) if (num >= R[k]) return k + convert(num -= R[k]);
 }
 
 convert(44);
