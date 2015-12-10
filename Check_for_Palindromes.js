51f26b7b7f2f507753cf7312032e39126b516445
Geoff Storbeck <storbeck@gmail.com>
2015-12-09 20:36:47 -0500

    condense code

--- Check_for_Palindromes.js
+++ Check_for_Palindromes.js
@@ -1,13 +1,7 @@
 function palindrome(str) {
-
-  arr = str.toLowerCase()
-           .replace(/[^a-z0-9]+/g, '')
-           .split('');
+  arr = str.toLowerCase().replace(/[^a-z0-9]+/g, '').split('');
   
-  var rev = arr.slice();
-  rev.reverse();
-  
-  return rev.every(function(e, i){
+  return arr.slice().reverse().every(function(e, i){
     return (e === arr[i]);
   });
   
