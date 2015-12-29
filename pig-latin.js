7c447699519e82aa0c1a6f7b582f1c57f2003d43
Geoff Storbeck <storbeck@gmail.com>
2015-12-29 16:14:19 -0500

    updated pig latin

--- pig-latin.js
+++ pig-latin.js
@@ -1,23 +1,6 @@
 function translate(str) {
-  var words = str.split('');
-  var vowels = ['a','e','i','o','u'];
-
-  // append way
-  if (vowels.indexOf(words[0]) >= 0) {
-    return str + 'way';
-  } else {
-    var final = [];
-    for (var i = 0; i < words.length; i++) {
-      // found vowel, stop moving around
-      if (vowels.indexOf(words[i]) >= 0) {
-        var word = words.join('').substr(i, words.length) + final.join('');
-        return word + 'ay';
-      } else {
-        final.push(words[i]);
-      }
-    }
-  }
- return str;
+  var pig = str.replace(/^([^aeiou]+)(.+)/, '$2$1ay');
+  return (pig === str) ? pig + 'way' : pig;
 }
 
-translate("consonant");
+translate("algorithm");
